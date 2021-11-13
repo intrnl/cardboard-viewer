10b1ff44b9dbfdac34df1564a514af0416d50459
intrnl <intrnl28+git@gmail.com>
2021-11-13 22:35:10 +0700

    log branch and commit

--- src/app.jsx
+++ src/app.jsx
@@ -6,4 +6,11 @@ import '~/src/styles/reset.css';
 import { App } from '~/src/components/App.jsx';
 
 
+const CF_COMMIT = import.meta.env.CF_PAGES_COMMIT_SHA;
+const CF_BRANCH = import.meta.env.CF_PAGES_BRANCH;
+
+if (CF_COMMIT && CF_BRANCH) {
+	console.debug(`running ${CF_BRANCH}/${CF_COMMIT.slice(0, 7)}`);
+}
+
 render(<App />, document.getElementById('root'));

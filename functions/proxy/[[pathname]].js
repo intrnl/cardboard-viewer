0e092f26f1ca02ab6a9b94c11722d8e38b870ff9
intrnl <intrnl28+git@gmail.com>
2021-11-17 22:47:16 +0700

    add debug headers

--- functions/proxy/[[pathname]].js
+++ functions/proxy/[[pathname]].js
@@ -4,14 +4,17 @@ export async function onRequest ({ request, params }) {
 	const { pathname } = params;
 	const { search } = new URL(request.url);
 
-	const destination = `${BASE_URL}/${pathname}${search}`;
+	const method = request.method;
 
-	console.log(`proxy: /${pathname}`);
+	const destination = `${BASE_URL}/${pathname}${search}`;
 
 	request = new Request(destination, request);
 	request.headers.set('Origin', BASE_URL);
 
-	const response = await fetch(request);
+	let response = await fetch(request);
+
+	response = new Response(response.body, response);
+	response.headers.set('x-proxy-debug', JSON.stringify({ pathname, search, method }))
 
 	return response;
 }

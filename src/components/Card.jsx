0924520735b85a9ccf31f86206a7d13fc3b006fc
intrnl <intrnl28+git@gmail.com>
2021-11-20 17:52:38 +0700

    card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,23 @@
+import { h } from 'preact';
+import { forwardRef } from 'preact/compat';
+
+import clsx from 'clsx';
+import * as styles from '~/src/styles/components/Card.module.css';
+
+
+const CardRef = forwardRef(Card);
+
+function Card (props, ref) {
+	const {
+		as = 'div',
+		className,
+		...rest
+	} = props;
+
+	const cn = clsx(styles.card, className);
+
+
+	return h(as, { ref, className: cn, ...rest });
+}
+
+export { CardRef as Card };

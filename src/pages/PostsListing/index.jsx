e06a6b375902180ca6c1c888e6ee714619ff842e
intrnl <intrnl28+git@gmail.com>
2021-12-07 21:11:08 +0700

    move postslisting to rq

--- src/pages/PostsListing/index.jsx
+++ src/pages/PostsListing/index.jsx
@@ -1,7 +1,7 @@
 import { h } from 'preact';
 import { Suspense } from 'preact/compat';
-import { useMemo } from 'preact/hooks';
-import { createMappedResource, createResource } from '~/lib/use-asset';
+import { useMemo, useState } from 'preact/hooks';
+import { useQuery } from '@intrnl/rq';
 import { useStore } from '~/lib/global-store';
 
 import { SideView, Main, Aside } from '~/layouts/SideView';
@@ -12,9 +12,10 @@ import { CircularProgress } from '~/components/CircularProgress';
 import * as styles from './PostsListing.css';
 
 import { AuthStore } from '~/globals/auth.js';
-import * as asset from '~/api/assets.js';
+import { getPostList, getPostCount, getTag, getPopularTags, getRelatedTags } from '~/api/assets.new';
 import { GET_MAX_PAGE } from '~/api/enums.js';
 
+import { createMappedResource, createResource } from '~/utils/resource';
 import { useSearchParams } from '~/utils/useSearchParams.js';
 import { useFactoryMemo } from '~/utils/useFactoryMemo.js';
 import { qss } from '~/utils/qss.js';
@@ -31,14 +32,16 @@ const RE_ORDER_RANDOM = /\border:random\b/i;
 export default function PostsListingPage () {
 	const [{ query, page, limit }, setParams] = useSearchParams(DEFAULT_SEARCH_PARAMS);
 
+	const auth = useStore(AuthStore);
+	const RE_ORDFAV = auth.profile && (auth.profile._re_ordfav ||= new RegExp(`\\b(?:ord)?fav:${auth.profile.name}\\b`));
+	const maxPage = GET_MAX_PAGE(auth.profile?.level);
+
 	const tags = useFactoryMemo(normalizeTags, [query]);
 	const pageNum = parseInt(page);
 	const limitNum = parseInt(limit);
 
 	const isOrderRandom = useMemo(() => RE_ORDER_RANDOM.test(tags), [tags]);
-	const posts = asset.postList.use({ tags, page: pageNum, limit: limitNum });
-	const count = asset.postCount.use(tags);
-	const tagsList = tags ? asset.relatedTags.use(tags) : asset.popularTags.use();
+	const isInFavorite = useMemo(() => RE_ORDFAV?.test(tags), [tags]);
 
 	const search = query ? qss({ query }) : false;
 
@@ -51,27 +54,27 @@ export default function PostsListingPage () {
 	return (
 		<SideView flow className={styles.container}>
 			<Main className={styles.main}>
-				<Suspense fallback={<PostsListingFallback size={limit} />}>
-					<PostsListing
-						resource={posts}
-						search={search}
-					/>
-				</Suspense>
+				<PostsListing
+					tags={tags}
+					page={pageNum}
+					limit={limitNum}
+					search={search}
+					isInFavorite={isInFavorite}
+				/>
 				{!isOrderRandom && (
-					<Suspense fallback={<PostsPaginationFallback />}>
-						<PostsPagination
-							resource={count}
-							page={pageNum}
-							limit={limitNum}
-							onChangePage={handlePageChange}
-						/>
-					</Suspense>
+					<PostsPagination
+						tags={tags}
+						page={pageNum}
+						maxPage={maxPage}
+						limit={limitNum}
+						onChangePage={handlePageChange}
+					/>
 				)}
 			</Main>
 
 			<Aside>
 				<Suspense fallback={<CircularProgress />}>
-					<TagsList resource={tagsList} />
+					<TagsList tags={tags} />
 				</Suspense>
 			</Aside>
 		</SideView>
@@ -84,9 +87,34 @@ function normalizeTags (tags) {
 
 // <TagsList />
 function TagsList (props) {
-	const { resource } = props;
+	const { tags } = props;
 
-	const data = resource.read();
+	const [prevDate] = useState(() => {
+		const date = new Date();
+		date.setDate(date.getDate() - 1);
+
+		return date.toISOString().split('T')[0];
+	});
+
+	const { data } = useQuery({
+		key: tags ? ['tag/related', tags] : ['tag/popular', prevDate],
+		fetch: tags ? getRelatedTags : getPopularTags,
+		staleTime: 60000,
+		suspense: true,
+	});
+
+	const createTagResource = (name) => {
+		return createResource(() => {
+			const { data } = useQuery({
+				key: ['tag', name],
+				fetch: getTag,
+				staleTime: 60000,
+				suspense: true,
+			});
+
+			return data;
+		});
+	}
 
 	return (
 		<div>
@@ -96,7 +124,7 @@ function TagsList (props) {
 					<Tag
 						key={tag}
 						as='li'
-						resource={createResource(() => asset.tags.read(tag))}
+						resource={createTagResource(tag)}
 					/>
 				))}
 			</ul>
@@ -106,10 +134,19 @@ function TagsList (props) {
 
 // <PostsListing />
 function PostsListing (props) {
-	const { resource, search } = props;
+	const { tags, page, limit, search, isInFavorite } = props;
 
-	const data = resource.read();
+	const { status, data } = useQuery({
+		key: ['post/list', { tags, page, limit }],
+		fetch: getPostList,
+		staleTime: 60000,
+	})
 
+	if (status === 'loading') {
+		return (
+			<PostsListingFallback size={limit} />
+		)
+	}
 
 	return (
 		<div className={styles.listing}>
@@ -119,6 +156,7 @@ function PostsListing (props) {
 					resource={createMappedResource(item)}
 					className={styles.item}
 					search={search}
+					isInFavorite={isInFavorite}
 				/>
 			))}
 		</div>
@@ -140,14 +178,21 @@ function PostsListingFallback (props) {
 
 // <PostsPagination />
 function PostsPagination (props) {
-	const { resource, page, limit, onChangePage } = props
+	const { tags, page, maxPage, limit, onChangePage } = props
 
-	const auth = useStore(AuthStore);
-	const count = resource.read();
+	const { status, data } = useQuery({
+		key: ['post/count', tags],
+		fetch: getPostCount,
+		staleTime: 60000,
+	});
+
+	if (status === 'loading') {
+		// Don't think we need to show placeholders for this.
+		return null;
+	}
 
 	const currentPage = page;
-	const totalPage = Math.max(Math.min(Math.ceil(count / limit), GET_MAX_PAGE(auth.profile?.level)), 1);
-
+	const totalPage = Math.max(Math.min(Math.ceil(data / limit), maxPage), 1);
 
 	return (
 		<Pagination
@@ -156,9 +201,4 @@ function PostsPagination (props) {
 			onChangePage={onChangePage}
 		/>
 	);
-}
-
-function PostsPaginationFallback () {
-	// I don't think we have an actual need to show placeholders for this.
-	return null;
 }

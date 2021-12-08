968551dfdd6f788de0b60fbe9fab3f070f8ef2b8
intrnl <intrnl28+git@gmail.com>
2021-12-08 10:10:48 +0700

    error boundary component

--- src/components/ErrorBoundary/index.jsx
+++ src/components/ErrorBoundary/index.jsx
@@ -1,1 +1,26 @@
-export { useErrorBoundary } from './useErrorBoundary';
+import { h } from 'preact';
+import { useState, useErrorBoundary } from 'preact/hooks';
+
+
+export function ErrorBoundary (props) {
+	const { children, fallback } = props;
+
+	const [error, setError] = useState();
+	useErrorBoundary(setError);
+
+	if (error) {
+		return h(fallback, { error, reset: () => setError() });
+	}
+
+	return children;
+}
+
+export function useBoundError (givenError) {
+	const [error, setError] = useState(givenError);
+
+	if (error) {
+		throw error;
+	}
+
+	return setError;
+}

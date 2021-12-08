d26e8959d56f77baf2d3d8e616b577f65fc3ebd3
intrnl <intrnl28+git@gmail.com>
2021-12-08 10:11:13 +0700

    put error container in the app

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,12 @@
 import { h } from 'preact';
+import { useEffect } from 'preact/hooks';
 import { Suspense } from 'preact/compat';
 
 import { Router } from './Router';
 import { AppRouter } from './AppRouter';
+import { ErrorBoundary } from '~/components/ErrorBoundary';
 import { CircularProgress } from '~/components/CircularProgress';
+import { Button } from '~/components/Button';
 import * as styles from './App.css';
 
 import { history } from '~/globals/history.js';
@@ -11,11 +14,13 @@ import { history } from '~/globals/history.js';
 
 export function App () {
 	return (
-		<Router history={history}>
-			<Suspense fallback={<AppFallback />}>
-				<AppRouter />
-			</Suspense>
-		</Router>
+		<ErrorBoundary fallback={ErrorContainer}>
+			<Router history={history}>
+				<Suspense fallback={<AppFallback />}>
+					<AppRouter />
+				</Suspense>
+			</Router>
+		</ErrorBoundary>
 	);
 }
 
@@ -23,6 +28,29 @@ function AppFallback () {
 	return (
 		<div className={styles.fallback}>
 			<CircularProgress />
+		</div>
+	);
+}
+
+function ErrorContainer (props) {
+	const { error, reset } = props;
+
+	const name = error?.name || 'Unspecified error';
+	const message = error?.message || error || 'No message provided';
+
+	useEffect(() => {
+		console.log('An error occured');
+		console.error(error);
+	}, [error]);
+
+	return (
+		<div className={styles.error}>
+			<h3>Whoops, something happened.</h3>
+			<pre><b>{name}</b>: {message}</pre>
+
+			<Button onClick={reset}>
+				Try again
+			</Button>
 		</div>
 	);
 }
